7c7e852107c9bc455388dc8ece6137ef1d19e600
Chibuzor Prince <chibuzorprnce68@gmail.com>
2025-03-31 09:31:47 -0700

    just checking changes

--- simple-storagef25/src/Counter.sol
+++ simple-storagef25/src/Counter.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    uint256 public numbers;
 
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        numbers = newNumber;
     }
 
     function increment() public {
-        number++;
+        numbers++;
     }
 }
