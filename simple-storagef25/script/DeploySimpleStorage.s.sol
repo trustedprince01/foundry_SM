4f6f964164ed9952299c6f29b64117a7319a5f30
trustedprince01 <chibuzorprince68@gmail.com>
2025-04-09 14:59:16 -0700

    Remove Counter contract and related scripts; add SimpleStorage contract and deployment script

---
+++ simple-storagef25/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external  returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+    }
+}
